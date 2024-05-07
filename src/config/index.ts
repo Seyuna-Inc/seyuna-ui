731b40300e1cdf71da6a40d9006a64befe2f73ad
Valy Aiken <hello@valyaiken.com>
2024-05-07 04:08:04 +0100

    init

---
+++ src/config/index.ts
@@ -0,0 +1,61 @@
+import { defaultTheme } from "@/theme/index.ts";
+import type { Config } from "./types.ts";
+
+/**
+ * Default configuration
+ *
+ * @returns Seyuna **Config** object
+ * @see {@link https://seyuna.com/ui/config#default}
+ */
+export const defaultConfig: Config = {
+    name: "Seyuna",
+    theme: "default",
+    mode: "system",
+    breakpoints: {
+        viewport: {
+            xs: "576px",
+            sm: "768px",
+            md: "992px",
+            lg: "1200px",
+            xl: "1600px",
+            _2xl: "1920px",
+            _3xl: "2560px",
+            _4xl: "3200px",
+            _5xl: "3840px",
+        },
+        container: {
+            xs: "100px",
+            sm: "200px",
+            md: "300px",
+            lg: "400px",
+            xl: "500px",
+            _2xl: "600px",
+            _3xl: "700px",
+            _4xl: "800px",
+            _5xl: "900px",
+            _6xl: "1000px",
+            _7xl: "1200px",
+            _8xl: "1400px",
+            _9xl: "1600px",
+            _10xl: "1800px",
+            _11xl: "2000px",
+            _12xl: "2200px",
+            _13xl: "2400px",
+            _14xl: "2600px",
+            _15xl: "2800px",
+            _16xl: "3000px",
+            _17xl: "3200px",
+            _18xl: "3400px",
+            _19xl: "3600px",
+            _20xl: "3800px",
+        },
+    },
+    scaling: "_2xl",
+    spacing: "1rem",
+    themes: [defaultTheme],
+};
+
+/**
+ * Export all types
+ */
+export type * from "./types.ts";
---
+++ src/theme/types.ts
@@ -0,0 +1,151 @@
+/**
+ * Seyuna theme object.
+ *
+ * See more: https://seyuna.com/ui/config/theme
+ */
+export type Theme = {
+    /**
+     * The name of the theme.
+     *
+     * See more: https://seyuna.com/ui/config/theme#name
+     */
+    name: string;
+
+    /**
+     * The color palette of the theme.
+     *
+     * See more: https://seyuna.com/ui/config/theme#palette
+     */
+    palette: Palette;
+};
+
+/**
+ * A color palette defined in OKLCH.
+ *
+ * See more: https://seyuna.com/ui/config/theme#palette
+ */
+export type Palette = {
+    /**
+     * Colors are defined using the hue point only in order to keep colors consistent in terms of lightness and chroma.
+     *
+     * See more: https://seyuna.com/ui/config/theme#palette-colors
+     */
+    colors: Colors;
+
+    /**
+     * The default color values for light mode.
+     *
+     * See more: https://seyuna.com/ui/config/theme#palette-light
+     */
+    light: {
+        /**
+         * The default background color for light mode.
+         */
+        background?: Color;
+
+        /**
+         * The default text color for the light mode.
+         */
+        textColor?: Color;
+
+        /**
+         * The default OKLCH lightness for all colors in light mode.
+         */
+        lightness: Lightness;
+
+        /**
+         * The default OKLCH chroma for all colors in light mode.
+         */
+        chroma: Chroma;
+    };
+
+    /**
+     * The default color values for dark mode.
+     *
+     * See more: https://seyuna.com/ui/config/theme#palette-dark
+     */
+    dark: {
+        /**
+         * The default background color for dark mode.
+         */
+        background?: Color;
+
+        /**
+         * The default text color for the dark mode.
+         */
+        textColor?: Color;
+
+        /**
+         * The default OKLCH lightness for all colors in dark mode.
+         */
+        lightness: Lightness;
+
+        /**
+         * The default OKLCH chroma for all colors in dark mode.
+         */
+        chroma: Chroma;
+    };
+};
+
+/**
+ * Pre-defined color mappings for the theme.
+ *
+ * See more: https://seyuna.com/ui/config/theme#colors
+ */
+export type Colors = {
+    alpha?: Hue;
+    beta?: Hue;
+    gamma?: Hue;
+    delta?: Hue;
+    epsilon?: Hue;
+    zeta?: Hue;
+    eta?: Hue;
+    theta?: Hue;
+    iota?: Hue;
+    kappa?: Hue;
+    lambda?: Hue;
+    mu?: Hue;
+    nu?: Hue;
+};
+
+/**
+ * Represents a color in OKLCH format.
+ *
+ * See more: https://seyuna.com/ui/config/theme#color
+ */
+export type Color = {
+    /**
+     * OKLCH Lightness Component
+     */
+    lightness?: Lightness;
+    /**
+     * OKLCH Chroma Component
+     */
+    chroma?: Chroma;
+    /**
+     * OKLCH Hue Component
+     */
+    hue?: Hue;
+};
+
+/**
+ * OKLCH Hue Component
+ */
+export type Hue = string;
+
+/**
+ * OKLCH Chroma Component
+ */
+export type Chroma = string;
+
+/**
+ * OKLCH Lightness Component
+ */
+export type Lightness = string;
+
+/**
+ * The mode of the theme.
+ *
+ * See more: https://seyuna.com/ui/config/theme#mode
+ */
+export type Mode = "light" | "dark" | "system" | "time";
